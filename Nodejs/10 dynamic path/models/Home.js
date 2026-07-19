e529d24a76349669352345e882455e5427d65f92
jyotismita-das-365 <jyotismitadas365@gmail.com>
2026-07-19 09:39:50 +0530

    P

--- Nodejs/10 dynamic path/models/Home.js
+++ Nodejs/10 dynamic path/models/Home.js
@@ -45,7 +45,11 @@ module.exports = class Home {
   static deleteById(homeId, callback) {
     Home.fetchAll(homes => {
       const newHomes = homes.filter(home => home.id !== homeId);
-      fs.writeFile(homeFilePath, JSON.stringify(newHomes), callback);
+      fs.writeFile(homeFilePath, JSON.stringify(newHomes), error => {
+        if(error){
+          console.log('error while deleting home',error);
+        }
+      });
     })
   }
 }
