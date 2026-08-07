7a844b5932fb369221ef927d587e5e19196133a9
jyotismita-das <jyotismitadas365@gmail.com>
2026-08-07 18:51:12 +0530

    P

--- np again/14 cookies and sessions/controllers/authController.js
+++ np again/14 cookies and sessions/controllers/authController.js
@@ -3,7 +3,11 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  console.log(req.isLoggedIn, req.body);
   res.cookie('isLoggedIn', true);
   res.redirect("/");
+}
+
+exports.postLogout = (req, res, next) => {
+  res.cookie('isLoggedIn', false);
+  res.redirect("/login");
 }
